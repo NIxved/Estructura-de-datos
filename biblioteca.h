cbc0baf106524b111f1611ff498265c0f9c608f9
David León <leoleonvelasquez@gmail.com>
2024-08-20 18:52:20 -0500

    Update biblioteca.h

--- biblioteca.h
+++ biblioteca.h
@@ -1,14 +1,17 @@
 using namespace std;
 
 int sumaRecursiva(int n);
+// Realiza una suma recursiva desde 1 hasta n, es decir, suma todos los números desde 1 hasta n utilizando recursividad.
 
 int cuadradosPares(int n);
+// Calcula la suma de los cuadrados de los números pares hasta n. Por ejemplo, si n es 4, podría sumar 2^2 + 4^2.
 
 int cuadradosPares2(int n);
+// Variación de la función anterior, con una lógica diferente para calcular la suma de los cuadrados de los números pares hasta n.
 
 int fibNumero(int n);
+//Devuelve el n número en la secuencia de Fibonacci. Si n es 5, devolvería el quinto número de Fibonacci.
 
 bool lineal(string s, char c, int l);
-
-
+// Busca el carácter 'c' en la cadena 's' comenzando desde la posición 'l' y devuelve verdadero si lo encuentra y falso si no.
 
